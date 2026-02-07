9da278cf9ddde9de47f22bfff2dc2f0453fc31cc
August Tranberg <augusttranbergspam@gmail.com>
2026-02-07 13:10:35 +0100

    Helper functions to print board

---
+++ pawnWizard.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+
+#include "pawnWizard.hpp"
+
+pawnWizard::pawnWizard() {}
+
+pawnWizard::~pawnWizard() {}
+
+char pawnWizard::getPieceAtSquare(const int bitSquare) {
+    /*
+    Helper function to get the piece type at a bitindexed square.
+    :param bitSquare: The bit indexed (0-63) square to query.
+    :return: A char representing the piece. lowercase is black, uppercase is white.
+    */
+
+    uint64_t mask = 1ULL << bitSquare;
+
+    if (whitePawns & mask) return 'P';
+    if (whiteKnights & mask) return 'N';
+    if (whiteBishops & mask) return 'B';
+    if (whiteRooks & mask) return 'R';
+    if (whiteQueens & mask) return 'Q';
+    if (whiteKing & mask) return 'K';
+    
+    if (blackPawns & mask) return 'p';
+    if (blackKnights & mask) return 'n';
+    if (blackBishops & mask) return 'b';
+    if (blackRooks & mask) return 'r';
+    if (blackQueens & mask) return 'q';
+    if (blackKing & mask) return 'k';
+
+    return '.'; // Empty square
+}
+
+void pawnWizard::printBoard() {
+    /*
+    Helper function to print the current board layout to terminal.
+    */
+
+    for(int row = 7; row >= 0; row--){
+        std::cout << (row + 1) << "| ";
+        for(int file = 0; file < 8; file++){
+            int bitSquare = row * 8 + file;
+            std::cout << getPieceAtSquare(bitSquare) << " ";
+        }
+        std::cout << "|" << "\n";
+    }
+
+    std::cout << "  -----------------\n";
+    std::cout << "   a b c d e f g h\n";
+}
+
+void pawnWizard::printSingleBitBoard(uint64_t &board) {
+    /*
+    Helper function to print a single piece-board to terminal.
+    */
+    
+    for (int rank = 7; rank >= 0; rank--) {
+        for (int file = 0; file < 8; file++) {
+            int square = rank * 8 + file;
+            std::cout << ((board >> square) & 1) << " ";
+        }
+        std::cout << "\n";
+    }
+    std::cout << "\n";
+}
--- pawnWizard.hpp
+++ pawnWizard.hpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <cstdint>
 #include <string>
 #include <cctype>
@@ -6,7 +5,13 @@
 class pawnWizard {
 
 public:
-    pawnWizard(/* args */);
+    pawnWizard();
+
+    // Testing functions
+    char getPieceAtSquare(const int bitSquare);
+    void printBoard();
+    void printSingleBitBoard(uint64_t &board);
+
     ~pawnWizard();
 
 protected:
@@ -15,13 +20,15 @@ protected:
     uint64_t whiteKnights = 0x0000000000000042;
     uint64_t whiteBishops = 0x0000000000000024;
     uint64_t whiteKing = 0x0000000000000008;
-    uint64_t whiteQueen = 0x0000000000000010;
+    uint64_t whiteQueens = 0x0000000000000010;
 
     uint64_t blackPawns = 0x00FF000000000000;
     uint64_t blackRooks = 0x8100000000000000;
     uint64_t blackKnights = 0x4200000000000000;
     uint64_t blackBishops = 0x24000000000000;
     uint64_t blackKing = 0x0800000000000000;
-    uint64_t blackQueen = 0x1000000000000000;
+    uint64_t blackQueens = 0x1000000000000000;
+
+    bool whiteToMove = true;
     
 };
